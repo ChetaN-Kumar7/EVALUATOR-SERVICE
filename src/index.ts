546e21477c2b2ef0cd196fe1266054dcc0264530
ChetaN-Kumar7 <chetankumar3018@gmail.com>
2026-01-25 11:48:13 +0530

    Basic TS + Express project setup done

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import serverConfig from "./config/serverConfig";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("TypeScript + Express working 🚀");
+});
+
+app.listen(serverConfig.PORT, () => {
+  console.log(`Server running on http://localhost:${serverConfig.PORT}`);
+});
+ 
